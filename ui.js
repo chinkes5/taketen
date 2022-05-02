3fa4a92e7e82604d2505a64b83921437a3f6ccf5
John <24719595+chinkes5@users.noreply.github.com>
2022-05-01 19:54:53 -0600

    a stab at roughing out what I need to do here

---
+++ ui.js
@@ -0,0 +1,8 @@
+function updatePlayer(){
+    //update the display to a form field to enter a new name
+    //save the name and update the page with new name
+};
+
+function saveScore(){
+    //hit the cloudflare worker with score and player name (but subtract the edit button)
+};
